869e2b41e12470a3a03a2e3bd0f9d42015da0048
Michael Jennings <mjennings061@gmail.com>
2020-11-16 18:45:24 +0000

    Updated pin assignments for a Nano

--- main_rf69/main_rf69.ino
+++ main_rf69/main_rf69.ino
@@ -1,4 +1,10 @@
-// rfm69hcw RF module comms
+/*
+ * Primary Engineer - Road Flash Project
+ * Authors - Michael Jennings, Andrew Wilson, Jordan Smart
+ * 
+ * Road-narrowing oncoming traffic warning system
+ * Compiled for the Arduino Nano
+ */
 
 #include <SPI.h>
 #include <RH_RF69.h>
@@ -6,12 +12,12 @@
 /************ Radio Setup ***************/
 //https://learn.adafruit.com/adafruit-rfm69hcw-and-rfm96-rfm95-rfm98-lora-packet-padio-breakouts/using-the-rfm69-radio
 #define RF69_FREQ 434.0
-#define RFM69_CS      4
-#define RFM69_INT     5
-#define RFM69_RST     2
-#define RFM69_LED     6
+#define RFM69_CS      9
+#define RFM69_INT     2
+#define RFM69_RST     8
+//#define RFM69_LED     13
 #define LED           7
-#define PIR           0
+#define PIR           3 // INT1 (D3)
 
 RH_RF69 rf69(RFM69_CS, RFM69_INT); // Singleton instance of the radio driver
 int16_t packetnum = 0;  // packet counter, we increment per xmission
@@ -30,11 +36,12 @@ const long timeoutInterval = 10000;
 boolean timeout = 0;
 
 
-void setup(){
+void setup() {
   Serial.begin(115200);
   rf69_setup();
   pinMode(LED, OUTPUT);
   digitalWrite(LED, 0);
+  pinMode(PIR, INPUT);
   attachInterrupt(digitalPinToInterrupt(PIR), triggered, FALLING);
 }
 
@@ -42,7 +49,7 @@ void setup(){
 void loop() {
   sendWhenTriggered();  // check for a local trigger to broadcast trigger
   rxTrigger = rf69_rxTrigger(); // check if a 'triggered' message has been received
-  if(rxTrigger){  // if a trigger has been received
+  if (rxTrigger) { // if a trigger has been received
     flashing = 1; // start flashing
   }
   ledControl(); // control LED flashing
@@ -56,10 +63,10 @@ void loop() {
 
 
 // check if the hardware interrupt is triggered and send an RF char
-void sendWhenTriggered(void){
-  if(trigger){
+void sendWhenTriggered(void) {
+  if (trigger) {
     detachInterrupt(digitalPinToInterrupt(PIR));
-    if(!flashing){  // only tell the other device to flash when currently off
+    if (!flashing) { // only tell the other device to flash when currently off
       char toSend[20] = "Triggered";
       boolean success = rf69_sendChar(toSend);
     }
@@ -70,9 +77,9 @@ void sendWhenTriggered(void){
 
 
 // control LED flashing
-void ledControl(void){
-  if(flashing){ //if the system has been told to start flashing
-    if(firstFlash){ //if it is the first flash of LED
+void ledControl(void) {
+  if (flashing) { //if the system has been told to start flashing
+    if (firstFlash) { //if it is the first flash of LED
       timeoutMillisPrevious = millis();
       firstFlash = 0;
     }
@@ -104,9 +111,9 @@ void ledControl(void){
 }//end ledControl
 
 
-boolean rf69_rxTrigger(void){
+boolean rf69_rxTrigger(void) {
   if (rf69.available()) {
-    // Should be a message for us now   
+    // Should be a message for us now
     uint8_t buf[RH_RF69_MAX_MESSAGE_LEN]; // received message buffer
     uint8_t len = sizeof(buf);
     if (rf69.recv(buf, &len)) {
@@ -129,32 +136,33 @@ boolean rf69_rxTrigger(void){
       Serial.println("\t Receive failed");
       return 0;
     }
-  } else{
+  } else {
     return 0;
   }
 }
 
 
-void sendAck(void){
+void sendAck(void) {
   // Send ACK!
   uint8_t data[] = "1";
   rf69.send(data, sizeof(data));
   rf69.waitPacketSent();
   Serial.println("\t Sent ACK");
-  Blink(RFM69_LED, 40, 3); //blink LED 3 times, 40ms between blinks
+  //  Blink(RFM69_LED, 40, 3); //blink LED 3 times, 40ms between blinks
 }
 
 
 // ISR
-void triggered(void){
+void triggered(void) {
   trigger = 1;
+  Serial.println("Trigger");
 }
 
 
-boolean rf69_sendChar(char radiopacket[20]){
-  itoa(packetnum++, radiopacket+13, 10);
+boolean rf69_sendChar(char radiopacket[20]) {
+  itoa(packetnum++, radiopacket + 13, 10);
   Serial.print("Sending "); Serial.print(radiopacket);
-  
+
   // Send a message!
   rf69.send((uint8_t *)radiopacket, strlen(radiopacket));
   rf69.waitPacketSent();
@@ -162,12 +170,12 @@ boolean rf69_sendChar(char radiopacket[20]){
   // Now wait for a reply
   uint8_t buf[RH_RF69_MAX_MESSAGE_LEN];
   uint8_t len = sizeof(buf);
-  if (rf69.waitAvailableTimeout(500))  { 
-    // Should be a reply message for us now   
+  if (rf69.waitAvailableTimeout(500))  {
+    // Should be a reply message for us now
     if (rf69.recv(buf, &len)) {
       Serial.print("\t ACK ");
       Serial.println((char*)buf);
-      Blink(RFM69_LED, 50, 3); //blink RFM69_LED 3 times, 50ms between blinks
+      //      Blink(RFM69_LED, 50, 3); //blink RFM69_LED 3 times, 50ms between blinks
       return 1;
     } else {
       Serial.println("\t Receive failed");
@@ -180,8 +188,8 @@ boolean rf69_sendChar(char radiopacket[20]){
 }
 
 
-void rf69_setup(void){
-  pinMode(RFM69_LED, OUTPUT);     
+void rf69_setup(void) {
+  //  pinMode(RFM69_LED, OUTPUT);
   pinMode(RFM69_RST, OUTPUT);
   digitalWrite(RFM69_RST, LOW);
 
@@ -193,7 +201,7 @@ void rf69_setup(void){
   delay(10);
   digitalWrite(RFM69_RST, LOW);
   delay(10);
-  
+
   if (!rf69.init()) {
     Serial.println("RFM69 radio init failed");
     while (1);
@@ -211,19 +219,20 @@ void rf69_setup(void){
 
   // The encryption key has to be the same as the one in the server
   uint8_t key[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
-                    0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
+                    0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08
+                  };
   rf69.setEncryptionKey(key);
-  
-  pinMode(RFM69_LED, OUTPUT);
+
+  //  pinMode(RFM69_LED, OUTPUT);
 
   Serial.print("RFM69 radio @");  Serial.print((int)RF69_FREQ);  Serial.println(" MHz");
 }
 
 void Blink(byte PIN, byte DELAY_MS, byte loops) {
-  for (byte i=0; i<loops; i++)  {
-    digitalWrite(PIN,HIGH);
+  for (byte i = 0; i < loops; i++)  {
+    digitalWrite(PIN, HIGH);
     delay(DELAY_MS);
-    digitalWrite(PIN,LOW);
+    digitalWrite(PIN, LOW);
     delay(DELAY_MS);
   }
 }
